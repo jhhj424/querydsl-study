6437b36282b4871bf49f8e833753c51b87f04c10
jiho shin <jhhj424@naver.com>
2021-03-12 16:28:39 +0900

    test: Querydsl Q-Type static import 해서 간략화 하기
    같은 테이블을 join 해야 할 경우, 명시적으로 객체를 생성해서 사용해야함

--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -12,6 +12,7 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -71,12 +72,10 @@ class MemberTest {
 
     @Test
     public void startQuerydsl() {
-        QMember m = new QMember("m");
-
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1")) // 파라미터 바인딩 처리
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1")) // 파라미터 바인딩 처리
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
