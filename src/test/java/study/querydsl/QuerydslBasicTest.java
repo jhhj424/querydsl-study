0be69c7ad5fdf18b279fb3e4799ce1ba433ef2e9
jiho shin <jhhj424@naver.com>
2021-03-17 15:15:32 +0900

    test: Querydsl - 조인 on절 학습 테스트

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -241,4 +241,47 @@ public class QuerydslBasicTest {
                 .extracting("username")
                 .containsExactly("TeamA", "TeamB");
     }
+
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이, TeamA인 팀만 조인, 회원은 모두 조회
+     */
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("TeamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+
+        assertThat(result.size()).isEqualTo(4);
+    }
+
+    /**
+     * 연관관계 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("TeamA"));
+        em.persist(new Member("TeamB"));
+        em.persist(new Member("TeamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team)
+                .on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+
+        assertThat(result.size()).isEqualTo(7);
+    }
 }
