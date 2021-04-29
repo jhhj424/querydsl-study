6f79d2c17e2bdf21194f200cd3be30af386b0d87
jiho shin <jhhj424@naver.com>
2021-04-30 00:59:53 +0900

    test: Querydsl - 동적 쿼리 - SQL function 호출하기

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -703,4 +703,35 @@ public class QuerydslBasicTest {
 
         assertThat(count).isEqualTo(4);
     }
+
+    // SQL function 호출 : replace 예제
+    @Test
+    public void sqlFunction() {
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace', {0}, {1}, {2})",
+                        member.username, "member", "M"))
+                .from(member)
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(4);
+    }
+
+    // ANSI 표준 - 내장 함수 제공
+    @Test
+    public void sqlFunction2() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+//                .where(member.username.eq(Expressions.stringTemplate(
+//                        "function('lower', {0})", member.username)))
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+        assertThat(result.size()).isEqualTo(4);
+    }
 }
